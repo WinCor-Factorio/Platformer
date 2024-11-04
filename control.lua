38a874297294ded333c223becd1cccc1fe339eec
Mcm <6616239+Quazaka@users.noreply.github.com>
2024-11-04 21:20:18 +0100

    Working

--- control.lua
+++ control.lua
@@ -17,10 +17,25 @@ script.on_init(function()
     platform.apply_starter_pack()
 
     storage.platform = platform
+
     init_hub()
+    set_starting_items()
 
     game.forces["player"].technologies["space-platform"].researched = true;
 end)
+
+function set_starting_items()
+    if hub then
+        hub.insert({ name = "crusher", count = 2 })
+        hub.insert({ name = "asteroid-collector", count = 4 })
+        hub.insert({ name = "assembling-machine-1", count = 10 })
+        hub.insert({ name = "inserter", count = 50 })
+        hub.insert({ name = "solar-panel", count = 5 })
+        hub.insert({ name = "space-platform-foundation", count = 990 })
+        hub.insert({ name = "electric-furnace", count = 4 })
+        hub.insert({ name = "transport-belt", count = 100 })
+    end
+end
 
 function init_hub()
     platform = storage.platform
@@ -33,19 +48,6 @@ end)
 
 script.on_event(defines.events.on_player_created, function(e)
     game.players[e.player_index].enter_space_platform(platform)
-end)
-
-script.on_nth_tick(60000, function(e)
-    if hub then
-        hub.insert({ name = "crusher", count = 2 })
-        hub.insert({ name = "asteroid-collector", count = 4 })
-        hub.insert({ name = "assembling-machine-1", count = 10 })
-        hub.insert({ name = "inserter", count = 50 })
-        hub.insert({ name = "solar-panel", count = 5 })
-        hub.insert({ name = "space-platform-foundation", count = 990 })
-        hub.insert({ name = "electric-furnace", count = 4 })
-        hub.insert({ name = "transport-belt", count = 100 })
-    end
 end)
 
 script.on_event(defines.events.on_player_respawned, function(e)
