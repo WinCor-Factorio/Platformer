eeba071a89b21673696f1ab367d43e71714dac1c
Mcm <6616239+Quazaka@users.noreply.github.com>
2024-11-04 19:19:58 +0100

    Update data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,11 +1,32 @@
 -- Disable all technologies
---data.raw.technology["technology-name"].enabled = false;
+for _, tech in pairs(data.raw.technology) do
+    tech.enabled = false
+end
+
+-- modify technologies
+table.insert(data.raw.technology["electronics"].effects, {
+    type = "unlock-recipe",
+    recipe = "long-handed-inserter"
+})
 
 
--- Recipes
-local spacePlatformFoundation = data.raw.recipe["space-platform-foundation"];
+-- Enable selected technologies
+data.raw.technology["electronics"].enabled = true;
+data.raw.technology["automation-science-pack"].enabled = true;
 
-spacePlatformFoundation.ingredients = {
-    { type = "item", name = "iron-plate", amount = 10 } }
 
-spacePlatformFoundation.enabled = true;
+-- Altered Recipes
+data.raw.recipe["space-platform-foundation"].ingredients = {
+    { type = "item", name = "iron-plate", amount = 10 }
+}
+
+-- Enable starting items.
+data.raw.recipe["cargo-bay"].enabled = true
+data.raw.recipe["electric-furnace"].enabled = true
+data.raw.recipe["crusher"].enabled = true
+data.raw.recipe["asteroid-collector"].enabled = true
+data.raw.recipe["assembling-machine-1"].enabled = true
+data.raw.recipe["inserter"].enabled = true
+data.raw.recipe["solar-panel"].enabled = true
+data.raw.recipe["steel-plate"].enabled = true
+data.raw.recipe["space-platform-foundation"].enabled = true
