9e2308f1693ab7daeabbe93afc7d125322c65bdb
Xiroc <XirocWinde@gmail.com>
2024-11-04 20:37:10 +0100

    Moved void to seperate file

--- data.lua
+++ data.lua
@@ -1,8 +1,1 @@
-local planet = table.deepcopy(data.raw["planet"]["nauvis"])
-
-planet.name = "void"
-planet.distance = 20
-planet.orientation = 0.5
-planet.localised_name = "Void"
-planet.asteroid_spawn_influence = 0.1
-data:extend { planet }
+require("__Platformer__/planets/void.lua")
