a1b0764443fb2f16ed22bea408a923e7ee6297fc
Xiroc <XirocWinde@gmail.com>
2024-11-11 21:40:18 +0100

    Added the posibility for crudeic-astoroid-chunk to spawn on research

--- control.lua
+++ control.lua
@@ -1,3 +1,4 @@
 -- gameplay
 require("__platformer__/control/init.lua")
 require("__platformer__/control/losing.lua")
+require("__platformer__/control/on_research_finished.lua")
--- data/prototypes/planets/void.lua
+++ data/prototypes/planets/void.lua
@@ -5,15 +5,12 @@ planet.distance = 20
 planet.orientation = 0.5
 planet.localised_name = "Void"
 planet.asteroid_spawn_influence = 0.1
-planet.asteroid_spawn_definitions = {}
-local asteroid_definition = {
+planet.asteroid_spawn_definitions = {{
     asteroid = "metallic-asteroid-chunk",
     probability = 0.1,
     speed = 0.1,
-    angle_when_stopped = 0.25
-}
+    angle_when_stopped = 0.25,
+    type = "asteroid-chunk"
+}}
 
-asteroid_definition.type = "asteroid-chunk"
-
-table.insert(planet.asteroid_spawn_definitions, asteroid_definition)
 data:extend { planet }
