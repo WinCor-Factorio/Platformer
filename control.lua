cb17d4d2100e2e99f335bfb62b0c7ab51e5883b0
Xiroc <XirocWinde@gmail.com>
2024-11-21 19:08:11 +0100

    Created Nauvis spawn definition

--- control.lua
+++ control.lua
@@ -1,4 +1,3 @@
 -- gameplay
 require("__platformer__/control/init.lua")
 require("__platformer__/control/losing.lua")
-require("__platformer__/control/on_research_finished.lua")
--- data/prototypes/planets/nauvis.lua
+++ data/prototypes/planets/nauvis.lua
@@ -6,5 +6,19 @@ planet.asteroid_spawn_definitions = {
         speed = 0.1,
         angle_when_stopped = 0.25,
         type = "asteroid-chunk"
+    },
+    {
+        asteroid = "crudeic-asteroid-chunk",
+        probability = 0.05,
+        speed = 0.05,
+        angle_when_stopped = 0.25,
+        type = "asteroid-chunk"
+    },
+    {
+        asteroid = "oxide-asteroid-chunk",
+        probability = 0.05,
+        speed = 0.05,
+        angle_when_stopped = 0.25,
+        type = "asteroid-chunk"
     }
 }
