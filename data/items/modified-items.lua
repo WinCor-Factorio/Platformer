3620b17d206920e5b799d32ed4c88b131b3de7ca
Mcm <6616239+Quazaka@users.noreply.github.com>
2024-12-04 20:20:25 +0100

    Added steel chests

--- data/items/modified-items.lua
+++ data/items/modified-items.lua
@@ -1,2 +1,3 @@
 data.raw["assembling-machine"]["captive-biter-spawner"].surface_conditions = {}
 data.raw["lab"]["biolab"].surface_conditions = {}
+data.raw["container"]["steel-chest"].surface_conditions = {}
