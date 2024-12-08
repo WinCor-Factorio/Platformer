6ede5ebf9fdd90591f97d5736dac5528f1bdf6d9
Xiroc <XirocWinde@gmail.com>
2024-12-08 13:55:57 +0100

    Changed speed and probability for asteroid between planets

--- data/prototypes/space-connections/gleba-fulgora.lua
+++ data/prototypes/space-connections/gleba-fulgora.lua
@@ -7,25 +7,25 @@ spaceConnection.asteroid_spawn_definitions = {
             {
                 distance = 0,
                 probability = 0,
-                speed = 1,
+                speed = 0.00001,
                 angle_when_stopped = 0.25
             },
             {
                 distance = 0.1,
                 probability = 0.015,
-                speed = 0.040,
+                speed = 0.020,
                 angle_when_stopped = 0.25
             },
             {
                 distance = 0.9,
                 probability = 0.0005,
-                speed = 0.040,
+                speed = 0.020,
                 angle_when_stopped = 0.25
             },
             {
                 distance = 1,
                 probability = 0,
-                speed = 1,
+                speed = 0.00001,
                 angle_when_stopped = 0.25
             }
         }
@@ -36,25 +36,25 @@ spaceConnection.asteroid_spawn_definitions = {
             {
                 distance = 0,
                 probability = 0,
-                speed = 1,
+                speed = 0.00001,
                 angle_when_stopped = 0.25
             },
             {
                 distance = 0.1,
                 probability = 0.0005,
-                speed = 0.040,
+                speed = 0.020,
                 angle_when_stopped = 0.25
             },
             {
                 distance = 0.9,
                 probability = 0.015,
-                speed = 0.040,
+                speed = 0.020,
                 angle_when_stopped = 0.25
             },
             {
                 distance = 1,
                 probability = 0,
-                speed = 1,
+                speed = 0.00001,
                 angle_when_stopped = 0.25
             }
         }
