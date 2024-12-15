bf98242f03909e15f27a209ab7b99a22e3a8f953
Mcm <6616239+Quazaka@users.noreply.github.com>
2024-12-15 15:23:03 +0100

    Go through all data entries and remove surface conditions.

--- data/items/modified-items.lua
+++ data/items/modified-items.lua
@@ -1,25 +1,11 @@
-data.raw["assembling-machine"]["captive-biter-spawner"].surface_conditions = {}
-data.raw["lab"]["biolab"].surface_conditions = {}
-data.raw["container"]["steel-chest"].surface_conditions = {}
-data.raw["agricultural-tower"]["agricultural-tower"].surface_conditions = {}
-data.raw["rail-ramp"]["rail-ramp"].surface_conditions = {}
-data.raw["rail-support"]["rail-support"].surface_conditions = {}
-data.raw["straight-rail"]["straight-rail"].surface_conditions = {}
-data.raw["elevated-straight-rail"]["elevated-straight-rail"].surface_conditions = {}
-data.raw["elevated-half-diagonal-rail"]["elevated-half-diagonal-rail"].surface_conditions = {}
-data.raw["half-diagonal-rail"]["half-diagonal-rail"].surface_conditions = {}
-data.raw["curved-rail-a"]["curved-rail-a"].surface_conditions = {}
-data.raw["elevated-curved-rail-a"]["elevated-curved-rail-a"].surface_conditions = {}
-data.raw["elevated-curved-rail-b"]["elevated-curved-rail-b"].surface_conditions = {}
-data.raw["curved-rail-b"]["curved-rail-b"].surface_conditions = {}
-data.raw["train-stop"]["train-stop"].surface_conditions = {}
-data.raw["locomotive"]["locomotive"].surface_conditions = {}
-data.raw["cargo-wagon"]["cargo-wagon"].surface_conditions = {}
-data.raw["fluid-wagon"]["fluid-wagon"].surface_conditions = {}
-data.raw["boiler"]["boiler"].surface_conditions = {}
-
-
-
+for _, type_data in pairs(data.raw) do
+    for _, item in pairs(type_data) do
+        if item.surface_conditions ~= nil or item.surface_conditions then
+            log(item.item.name)
+            item.surface_conditions = nil
+        end
+    end
+end
 
 data.raw["rail-support"]["rail-support"].support_range = 50
 data.raw["rail-ramp"]["rail-ramp"].support_range = 50
