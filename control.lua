19b18f5967f8c795d5beced7e1ed8b171836d6de
Mathias Winde <Xiroc@users.noreply.github.com>
2025-01-09 20:35:54 +0100

    Removed lose condition

--- control.lua
+++ control.lua
@@ -1,5 +1,4 @@
 -- gameplay
 require("__platformer__/control/init.lua")
-require("__platformer__/control/losing.lua")
 require("__platformer__/control/events.lua")
 require("__platformer__/control/debug.lua")
